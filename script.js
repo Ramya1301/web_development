85a5b7bb9b6054a6ec30a1db47e3ab4c8c958c26
Ramyasrisanaboina <20a91a05b6@aec.edu.in>
2023-08-12 19:40:19 +0530

    project added

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputbox = document.getElementById("input_box");
+const listitems= document.getElementById("list_items");
+
+function add(){
+    if(inputbox.value == "")
+    {
+        alert("write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listitems.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+
+    }
+    inputbox.value="";
+    saveData();
+}
+listitems.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+       e.target.parentElement.remove();
+       saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listitems.innerHTML);
+}
+function show(){
+    listitems.innerHTML= localStorage.getItem("data");
+}
+show();
